da1eee49dc127e16626f6f572d1fbd0da23253a7
Administrator-SIMRS <rizky.maulana2351@gmail.com>
2024-12-14 22:59:49 +0700

    Sesi 10

---
+++ models/user-model.js
@@ -0,0 +1,22 @@
+const db = require("../config/db");
+
+const getAllUser = async () => {
+  const [rows] = await db.query("select * from siswa");
+  return rows;
+};
+
+const getUserById = async (id) => {
+  const [row] = await db.query("select * from siswa where id=?", id);
+  return row;
+};
+
+const updateUser = async (id, user) => {
+  const { name, email, phone } = user;
+  const [result] = await db.query(
+    " insert  into siswa (name, email, phone) values (?,?,?)",
+    [name, email, phone]
+  );
+  return result.insert.Id;
+};
+
+module.exports = { getAllUser, getUserById, updateUser };
